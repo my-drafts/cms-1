03edcabe47ee77a6b0ff71d29e2452d4737cdf92
Sergej Kukharev <sergej.kucharev@gmail.com>
2017-01-28 05:43:06 +0200

    0.0.22

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 'use strict';
 
-const Config = require('./lib/Config');
-const config = require('./config.json');
-const cfg = new Config();
-cfg.blocks = config;
-
 const Application = require('./lib/Application');
-const app = new Application();
-app.init(cfg);
+const app = new Application(require('./config.json'));
 app.run();
