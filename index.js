b3b7e9e4b9b2b22c63f1b26ddc9b84a340f4989d
Sergyi Kukharyev <sergej.kucharev@gmail.com>
2017-01-29 02:34:01 +0200

    0.0.29

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+#!/usr/bin/env node
 'use strict';
 
+const Debugger = require('./lib/Debugger');
 const Application = require('./lib/Application');
 const app = new Application(require('./config.json'));
 app.run();
