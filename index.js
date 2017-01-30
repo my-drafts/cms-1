b3e00b711707db0b33a6a068a3609c2abab40907
Sergyi Kukharyev <sergej.kucharev@gmail.com>
2017-01-30 23:21:09 +0200

    0.0.32

--- index.js
+++ index.js
@@ -2,6 +2,18 @@
 'use strict';
 
 const Debugger = require('./lib/Debugger');
+const D = Debugger.init({ level: 'all', owner: '/' });
+
+D.trace('creating Config');
+const Config = require('./lib/Config');
+const cfg = new Config(Debugger, require('./config.json'));
+D.trace('created Config');
+
+D.trace('creating Application');
 const Application = require('./lib/Application');
-const app = new Application(require('./config.json'), Debugger);
+const app = new Application(Debugger, cfg);
+D.trace('created Application');
+
+D.trace('runing Application');
 app.run();
+D.trace('runed Application');
